faea0b848f2560df367d32caddc0cfbf7960c327
kirill201 <kirill.301522@gmail.com>
2023-11-23 20:40:33 +0300

    _init_

--- backend/ai/fuzzy.py
+++
@@ -1,9 +0,0 @@
-from fuzzywuzzy import process
-
-def fuzzy_match(query, options: list, limit=10) -> list:
-    """
-    Получение похожего слова/запроса
-    """
-    matches = process.extract(query, options, limit=limit)
-
-    return matches
