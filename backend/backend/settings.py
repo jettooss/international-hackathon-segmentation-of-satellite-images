3224e8e25943d929506c442d2aa3537fc6731fe7
kirill201 <kirill.301522@gmail.com>
2023-11-24 01:06:55 +0300

    front

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -102,7 +102,7 @@ DEBUG = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
