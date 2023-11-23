9f982f3641f3cab37686830ae21519de4159b847
kirill201 <kirill.301522@gmail.com>
2023-11-24 00:41:52 +0300

    front

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
+from django.views.generic.base import TemplateView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
 
 from api.views import process_image
 
@@ -16,11 +20,11 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('ai/', process_image),
 
     path('api/swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
