0987d3ba5a9196269bf78d609886720374e8ec07
kirill201 <kirill.301522@gmail.com>
2023-11-25 01:12:20 +0300

    tenzoflow init

--- backend/ai/main.py
+++ backend/ai/main.py
@@ -1,16 +1,17 @@
 import cv2
 import numpy as np
 import os
-
-os.environ["SM_FRAMEWORK"] = "tf.keras"
+import concurrent.futures
 from tqdm import tqdm
 import segmentation_models as sm
 from tensorflow.keras.models import load_model
 
+os.environ["SM_FRAMEWORK"] = "tf.keras"
+
 
 def prediction(model, img, patch_size):
     """
-    Generate a segmentation mask for the input image using the model.
+    Generate a segmentation mask for the input image using the model with multi-threading.
 
     :param model: The loaded segmentation model.
     :param img: Input image.
@@ -20,42 +21,42 @@ def prediction(model, img, patch_size):
     stride_size = 128
     mask = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
     BACKBONE = 'mobilenet'
-
     preprocess_input = sm.get_preprocessing(BACKBONE)
-    total_patches = ((img.shape[0] - patch_size) // stride_size + 1) * ((img.shape[1] - patch_size) // stride_size + 1)
-    progress_bar = tqdm(total=total_patches, desc="Processing patches")
 
+    def process_patch(i, j, img, model, preprocess_input):
+        img_patch = img[i:i + patch_size, j:j + patch_size, :]
+        img_patch = preprocess_input(img_patch)
+        img_patch = np.expand_dims(img_patch, axis=0)
+        pred_mask = model.predict(img_patch)
+        pred_mask = np.argmax(pred_mask, axis=3)[0].astype('uint8')
+        return i, j, cv2.resize(pred_mask, (patch_size, patch_size))
+
+    patches = []
     for i in range(0, img.shape[0] - patch_size + 1, stride_size):
         for j in range(0, img.shape[1] - patch_size + 1, stride_size):
-            img_patch = img[i:i + patch_size, j:j + patch_size, :]
-            img_patch = preprocess_input(img_patch)
-            img_patch = np.expand_dims(img_patch, axis=0)
-            pred_mask = model.predict(img_patch)
-            pred_mask = np.argmax(pred_mask, axis=3)[0].astype('uint8')
-            pred_mask = cv2.resize(pred_mask, (patch_size, patch_size))
-            mask[i:i + patch_size, j:j + patch_size] = pred_mask
-            progress_bar.update(1)
+            patches.append((i, j))
 
-    progress_bar.close()
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(process_patch, i, j, img, model, preprocess_input) for i, j in patches]
+        for future in tqdm(concurrent.futures.as_completed(futures), total=len(futures), desc="Processing patches"):
+            i, j, pred_patch = future.result()
+            mask[i:i + patch_size, j:j + patch_size] = pred_patch
+
     return mask
 
 
-def create_overlay_image(original_img, mask, alpha=0.5):
+def overlay_mask_on_image(original_image: np.ndarray, mask: np.ndarray) -> np.ndarray:
     """
     Overlay the segmentation mask on the original image.
 
-    :param original_img: The original image.
+    :param original_image: The original image.
     :param mask: The segmentation mask.
-    :param alpha: Transparency level for the mask overlay.
     :return: Image with the mask overlay.
     """
-    # Convert mask to a 3 channel image
-    colored_mask = np.zeros_like(original_img)
-    for i in range(3):  # Assuming original_img has 3 channels
-        colored_mask[:, :, i] = mask
-
-    # Overlay the mask on the original image
-    overlayed_image = cv2.addWeighted(original_img, 1, colored_mask, alpha, 0)
+    alpha = 0.5  # Transparency factor for the mask
+    if len(mask.shape) == 2:
+        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    overlayed_image = cv2.addWeighted(original_image, 1, mask, alpha, 0)
     return overlayed_image
 
 
@@ -64,7 +65,6 @@ def process_image_for_segmentation(file_path: str):
     Process the image for segmentation.
 
     :param file_path: Path to the input image file.
-    :param model: Loaded segmentation model.
     """
     model = load_model('ai/model_20.h5', compile=False)
     try:
@@ -74,9 +74,7 @@ def process_image_for_segmentation(file_path: str):
 
         patch_size = 256
         predicted_mask = prediction(model, original_img, patch_size)
-
-        # Assuming overlayed_image is generated from original_img and predicted_mask
-        overlayed_image = create_overlay_image(original_img, predicted_mask)
+        overlayed_image = overlay_mask_on_image(original_img, predicted_mask)
         cv2.imwrite(file_path, overlayed_image)
 
     except Exception as e:
