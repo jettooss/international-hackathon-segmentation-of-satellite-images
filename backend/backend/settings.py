8a60b17afc0ce55eb58687f31dcb087fe668919f
kirill201 <kirill.301522@gmail.com>
2023-11-24 16:17:55 +0300

    refactor docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,16 +33,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost",
-    "http://localhost:3000",
-    'http://127.0.0.1:8000',
-]
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'http://localhost',
-    'http://127.0.0.1:8000',
-]
+
 CORS_ALLOW_HEADERS = [
     'accept',
     'accept-encoding',
