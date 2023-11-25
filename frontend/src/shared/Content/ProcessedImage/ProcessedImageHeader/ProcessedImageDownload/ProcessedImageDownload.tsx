3036961358502bf35acb0ef4a92658ac6dc2a037
Marian <roshchupkin.marian@gmail.com>
2023-11-25 12:54:57 +1000

    added download button logic

--- frontend/src/shared/Content/ProcessedImage/ProcessedImageHeader/ProcessedImageDownload/ProcessedImageDownload.tsx
+++ frontend/src/shared/Content/ProcessedImage/ProcessedImageHeader/ProcessedImageDownload/ProcessedImageDownload.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import styles from './processedimagedownload.module.css';
+import {IInitialState} from "../../../../../store/reducer";
+import {useSelector} from "react-redux";
+import useDownloader from 'react-use-downloader';
 
 export function ProcessedImageDownload() {
+  const processedImage = useSelector<IInitialState, string>(state => state.processedImage.processedImage);
+  const { download } = useDownloader();
+  const fileUrl = processedImage;
+  const filename = 'processed-image.jpg';
+
+  function handleClick() {
+    download(fileUrl, filename);
+  }
+
   return (
-    <button className={styles.button}>
+    <button className={styles.button} onClick={handleClick}>
       <span className={styles.desc}>Download</span>
     </button>
   );
